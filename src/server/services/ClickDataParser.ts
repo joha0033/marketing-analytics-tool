fc1c8a66e24967871479102b3f2758bcce7eda38
lucaswhitman <lucasmichaelwhitman@gmail.com>
2020-01-20 13:18:23 -0700

    finalize basic parser

--- src/server/services/ClickDataParser.ts
+++ src/server/services/ClickDataParser.ts
@@ -1,10 +1,19 @@
 import {EventEmitter} from 'events';
 import fs from 'fs';
 import {parse} from 'fast-csv';
-import {ClickData} from 'server/models/ClickData';
+import {ClickData} from '../models/ClickData';
+
+import path from 'path';
 
 class ClickDataParser {
   static parseFile(targetFile: string) {
+    // derive global per-file variables
+    const fileParts = path.basename(targetFile, '.csv').split('_');
+    const source = fileParts[0];
+    const month = parseInt(fileParts[1]);
+    const day = parseInt(fileParts[2]);
+    const year = parseInt(fileParts[3]);
+
     try {
       let stream = fs.createReadStream(targetFile);
       let csvData: string[] = [];
@@ -16,14 +25,21 @@ class ClickDataParser {
           // remove the first line: header
           csvData.shift();
 
-          csvData.forEach(row => {
-            console.log(row);
-            /*             const newData = new ClickData({
-              product: row.foo,
-              createdAt: req.body.createdAt,
-              source: req.body.source,
-              clicks: req.body.clicks
-            }); */
+          csvData.forEach(async row => {
+            // todo: this works with sample data...
+            // a more scalable solution would be to provide a UI
+            // and the ability for maps of file definitions
+            const product = row[0];
+            const clicks = row[1];
+
+            const newData = new ClickData({
+              product: product,
+              createdAt: new Date(year, month, day),
+              source: source,
+              clicks: clicks
+            });
+
+            await newData.save();
           });
         });
 
